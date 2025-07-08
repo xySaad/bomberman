6f5e22f5f5fb2ae08bb3de7a9410be00ba7ba0bb
fahdaguenouz <faguenouz@gmail.com>
2025-07-08 12:42:23 +0100

    changing clases name

--- client/app/pages/PlayGround.js
+++ client/app/pages/PlayGround.js
@@ -6,10 +6,10 @@ const HEIGHT = 15;
 // Cell types
 // 0: wall, 1: ground, 2: box, 3: unbreakable box
 const getClass = {
-    0: "cell wall",
-    1: "cell ground",
-    2: "cell box",
-    3: "cell unbreakable",
+    0: "wall",
+    1: "ground",
+    2: "box",
+    3: "unbreakable",
 };
 
 const isCornerSpawnZone = (x, y) => {
