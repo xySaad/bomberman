add1a714dac8039c0d7990b7867b13c61e250714
xySaad <saadrm.dev@gmail.com>
2025-07-15 19:19:20 +0100

    fix: move player by specified speed instead of block by block

--- server/game/player.js
+++ server/game/player.js
@@ -1,11 +1,12 @@
 import { User } from "../network/user.js";
+import { throttle } from "../utils/throttle.js";
 import { PLAYER_SPAWNS } from "./map.js";
 
 export class Player extends User {
   #nickname = null;
   #game = null;
-  #lastInputTime = 0;
-  #THROTTLE_DELAY = 300;
+  #speed = 0.2; // 0.2 per 100ms
+  #throttledMoveTo = throttle(100, (...args) => this.moveTo(...args));
   get nickname() {
     return this.#nickname;
   }
@@ -26,18 +27,18 @@ export class Player extends User {
   nextPosition(input) {
     const { x, y } = this.position;
     return {
-      ArrowUp: [x, y - 1],
-      ArrowDown: [x, y + 1],
-      ArrowRight: [x + 1, y],
-      ArrowLeft: [x - 1, y],
-      Space: [x, y],
+      ArrowUp: [x, y - 1 * this.#speed],
+      ArrowDown: [x, y + 1 * this.#speed],
+      ArrowRight: [x + 1 * this.#speed, y],
+      ArrowLeft: [x - 1 * this.#speed, y],
     }[input];
   }
   handleInput(input) {
     const nextPosition = this.nextPosition(input);
+    if (!nextPosition) return;
     const canMove = this.canMoveTo(...nextPosition);
-    if (nextPosition && canMove) {
-      this.moveTo(...nextPosition);
+    if (canMove) {
+      this.#throttledMoveTo(...nextPosition);
     }
   }
   moveTo(x, y) {
@@ -47,16 +48,15 @@ export class Player extends User {
       ...(this.position = { x, y }),
     });
   }
-  canMoveTo(x, y) {
+  canMoveTo(nextX, nextY) {
+    const { x: currentX, y: currentY } = this.position;
+    const x = currentX > nextX ? Math.floor(nextX) : Math.ceil(nextX);
+    const y = currentY > nextY ? Math.floor(nextY) : Math.ceil(nextY);
+
     // check if he can move to that block
     const { map } = this.#game;
     if (y < 0 || y >= map.length || x < 0 || x >= map[0].length) return false;
     if (map[y][x] !== 1) return false;
-
-    // throthle last moved block
-    const now = performance.now();
-    if (now - this.#lastInputTime < this.#THROTTLE_DELAY) return false;
-    this.#lastInputTime = now;
     return true;
   }
 }
